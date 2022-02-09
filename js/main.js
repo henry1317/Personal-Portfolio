11acd3fbe0ffdbe0ce7082f151a9d7d388267670
Henry Mberere <67532486+henry1317@users.noreply.github.com>
2022-02-10 01:46:29 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+
+
+$(function ($) {
+    "use strict";
+    jQuery(document).ready(function () {
+
+        // Typed js
+        $(".typed").typed({
+            strings: ["Web Developer.", "Graphic Designer.", "Freelancer."],
+            // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+            stringsElement: null,
+            // typing speed
+            typeSpeed: 30,
+            // time before typing starts
+            startDelay: 500,
+            // backspacing speed
+            backSpeed: 20,
+            // time before backspacing
+            backDelay: 1200,
+            // loop
+            loop: true
+        });
+    });
+    $('#testimonial-carousel').owlCarousel({
+        autoplay: true,
+        dots: false,
+        loop: true,
+        // margin:10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 2
+            }
+        }
+    });
+
+    $('#blog-carousel').owlCarousel({
+        autoplay: true,
+        dots: false,
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+
+
+    // $(window).on('load', function() {
+    //     new WOW().init();
+    // })
+
+
+});
+new WOW().init();
+// window.sr = ScrollReveal();
+//     sr.reveal('.icon-info', {
+//     duration: 2000,
+//     origin: 'top',
+//     distance: '300px'
+// });
+
+
+
+
+
